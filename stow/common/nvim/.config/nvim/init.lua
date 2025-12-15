3b2624dec31ad91848986c29d50eb39c3cdc8c3f
Unbinilium <15633984+Unbinilium@users.noreply.github.com>
2025-12-16 00:28:29 +0800

    Update nvim init

---
+++ stow/common/nvim/.config/nvim/init.lua
@@ -0,0 +1,10 @@
+vim.cmd([[
+  set runtimepath^=~/.vim runtimepath+=~/.vim/after
+  let &packpath = &runtimepath
+  source ~/.vimrc
+]])
+
+vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'Pmenu', { bg = 'none' })
--- stow/common/nvim/.config/nvim/init.vim
+++
@@ -1,6 +0,0 @@
-set runtimepath^=~/.vim runtimepath+=~/.vim/after
-let &packpath = &runtimepath
-source ~/.vimrc
-
-highlight Normal guibg=NONE ctermbg=NONE
-highlight NormalFloat guibg=NONE ctermbg=NONE
