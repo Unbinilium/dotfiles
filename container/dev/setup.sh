ad2b38f88b4cbba9fe54eb8d3243a0f8ed21caaa
Unbinilium <15633984+Unbinilium@users.noreply.github.com>
2025-12-28 22:13:33 +0800

    Skip stop builder

--- container/dev/setup.sh
+++ container/dev/setup.sh
@@ -12,8 +12,7 @@ ssh-keygen -y -f ${HOME}/.ssh/container > "$SCRIPT_DIR/authorized_keys"
 (
   cd "$SCRIPT_DIR/../.."
   container builder start --cpus 4 --memory 8g && \
-  container build --tag dev --file "$SCRIPT_DIR/Dockerfile" . && \
-  container builder stop
+  container build --tag dev --file "$SCRIPT_DIR/Dockerfile" .
 )
 
 unset SCRIPT_DIR
